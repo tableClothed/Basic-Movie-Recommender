766254baac545945843c37cf9041d7ac13fa607e
Weronika Żak <weronikarzak@gmail.com>
2020-01-25 02:07:08 +0100

    simple form

--- recommender/movies_rm/urls.py
+++ recommender/movies_rm/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("<int:id>", views.index, name="index")
+    path('<int:id>', views.index, name="index"),
+    path("", views.home, name="home"),
+    path("create/", views.create, name="index")
 ]
--- recommender/movies_rm/views.py
+++ recommender/movies_rm/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from .form import CreateNewList
 from .models import ToDoList, Item
 # Create your views here.
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1> %s </h1>" %ls.name)
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
+
+def create(request):
+    if request.method == "POST":
+        form = CreateNewList(request.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
+
+    return render(request, "main/create.html", {"form":form})
+    
