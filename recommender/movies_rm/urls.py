ef668a674f2162df3fd5a025980e7bac65e2e4c4
Weronika Żak <weronikarzak@gmail.com>
2020-01-26 01:35:12 +0100

    add bootstrap, save/add item

--- recommender/movies_rm/urls.py
+++ recommender/movies_rm/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('<int:id>', views.index, name="index"),
     path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("create/", views.create, name="index")
 ]
