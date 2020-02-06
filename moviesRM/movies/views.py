60a8a3eb07ec0ec204e62ad955258749ac05aee6
Weronika Żak <weronikarzak@gmail.com>
2020-02-07 00:20:20 +0100

    boostraping main page

--- moviesRM/movies/views.py
+++ moviesRM/movies/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import Movie
 
 def home(response):
-    return render(response, "main/home.html", {})
+    latest_movies = Movie.objects.order_by("popularity")[:9]
+    context = {"latest_movies":latest_movies}
+    return render(response, "main/home.html", context)
